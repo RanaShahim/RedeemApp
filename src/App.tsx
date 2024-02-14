6bde6c0bae586d8292be2641cd262a04ada3e1d6
Shahim <shahim123321@gmail.com>
2024-02-15 00:37:39 +1100

    fixed images

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,91 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import mapRomanceData from "./mappers/rommanceMapper";
-import Romance from "./classes/Romance";
+import { useEffect, useState } from 'react';
+import './App.css';
+import mapRomanceData from './mappers/rommanceMapper';
+import Romance from './classes/Romance';
 
 function App() {
-  const [romanceRecord, setRomanceRecord] = useState<Romance | null>(null);
-  const [hasAlreadyRedeemed, setHasAlreadyRedeemed] = useState(
-    JSON.parse(localStorage.getItem("hasAlreadyRedeemed") as any) ?? true
-  );
-  const [currentIndex, setCurrentIndex] = useState<number>(
-    (() => {
-      const storedIndex = localStorage.getItem("currentIndex");
-      return storedIndex ? parseInt(storedIndex, 10) : 2;
-    })()
-  );
-
-  const setRomanceData = (): void => {
-    const data = mapRomanceData();
-    const defaultRecord = data.find((record) => record?.getId() === "default");
-    const redeemableRecords = data.filter(
-      (record) => record?.getId() !== "default"
+    const [romanceRecord, setRomanceRecord] = useState<Romance | null>(null);
+    const [hasAlreadyRedeemed, setHasAlreadyRedeemed] = useState(
+        JSON.parse(localStorage.getItem('hasAlreadyRedeemed') as any) ?? true,
     );
-    if (!hasAlreadyRedeemed) {
-      const newRecord = redeemableRecords[currentIndex];
-      setRomanceRecord(newRecord);
-    } else {
-      if (defaultRecord) {
-        setRomanceRecord(defaultRecord);
-      }
-    }
-  };
+    const [currentIndex, setCurrentIndex] = useState<number>(
+        (() => {
+            const storedIndex = localStorage.getItem('currentIndex');
+            return storedIndex ? parseInt(storedIndex, 10) : 2;
+        })(),
+    );
 
-  const toggleApplicationState = (): void => {
-    setHasAlreadyRedeemed(false);
-    localStorage.setItem("hasAlreadyRedeemed", JSON.stringify(false));
-    resetState();
-  };
+    const setRomanceData = (): void => {
+        const data = mapRomanceData();
+        const defaultRecord = data.find(
+            (record) => record?.getId() === 'default',
+        );
+        const redeemableRecords = data.filter(
+            (record) => record?.getId() !== 'default',
+        );
+        if (!hasAlreadyRedeemed) {
+            const newRecord = redeemableRecords[currentIndex];
+            setRomanceRecord(newRecord);
+        } else {
+            if (defaultRecord) {
+                setRomanceRecord(defaultRecord);
+            }
+        }
+    };
 
-  const resetState = (): void => {
-    const now = new Date();
-    const tomorrow = new Date(now);
-    tomorrow.setDate(now.getDate() + 1);
-    tomorrow.setHours(0, 0, 0, 0);
-    const timeUntilTomorrow = tomorrow.getTime() - now.getTime();
-    setTimeout(() => {
-      const data = mapRomanceData();
-      const redeemableRecords = data.filter(
-        (record) => record?.getId() !== "default"
-      );
-      const newIndex = Math.floor(Math.random() * redeemableRecords.length);
-      setCurrentIndex(newIndex);
-      localStorage.setItem("currentIndex", newIndex.toString());
-      setHasAlreadyRedeemed(true);
-      localStorage.setItem("hasAlreadyRedeemed", JSON.stringify(true));
-    }, timeUntilTomorrow);
-  };
+    const toggleApplicationState = (): void => {
+        setHasAlreadyRedeemed(false);
+        localStorage.setItem('hasAlreadyRedeemed', JSON.stringify(false));
+        resetState();
+    };
 
-  useEffect(() => {
-    setRomanceData();
-    if (!hasAlreadyRedeemed) {
-      resetState();
-    }
-  }, [hasAlreadyRedeemed]);
+    const resetState = (): void => {
+        const now = new Date();
+        const tomorrow = new Date(now);
+        tomorrow.setDate(now.getDate() + 1);
+        tomorrow.setHours(0, 0, 0, 0);
+        const timeUntilTomorrow = tomorrow.getTime() - now.getTime();
+        setTimeout(() => {
+            const data = mapRomanceData();
+            const redeemableRecords = data.filter(
+                (record) => record?.getId() !== 'default',
+            );
+            const newIndex = Math.floor(
+                Math.random() * redeemableRecords.length,
+            );
+            setCurrentIndex(newIndex);
+            localStorage.setItem('currentIndex', newIndex.toString());
+            setHasAlreadyRedeemed(true);
+            localStorage.setItem('hasAlreadyRedeemed', JSON.stringify(true));
+        }, timeUntilTomorrow);
+    };
 
-  return (
-    <>
-      <div className="card">
-        <h3 className="heading">Daily Romance</h3>
-        <div className="image">
-          <img
-            src={`../src/assets/images/${romanceRecord?.getImageUrl()}`}
-          ></img>
-        </div>
-        <p className="text">~~ {romanceRecord?.getTitle()} ~~</p>
-        <p className="text">{`"${romanceRecord?.getQuote()}"`}</p>
-        <h3 className="text">{romanceRecord?.getRedeemedResult()}</h3>
-        {hasAlreadyRedeemed && (
-          <button className="button" onClick={toggleApplicationState}>
-            Redeem your everyday treat 🎉😘
-          </button>
-        )}
-      </div>
-    </>
-  );
+    useEffect(() => {
+        setRomanceData();
+        if (!hasAlreadyRedeemed) {
+            resetState();
+        }
+    }, [hasAlreadyRedeemed]);
+
+    return (
+        <>
+            <div className="card">
+                <h3 className="heading">Daily Romance</h3>
+                <div className="image">
+                    <img
+                        src={`https://redeeming-romance.netlify.app/src/assets/images/${romanceRecord?.getImageUrl()}`}
+                    ></img>
+                </div>
+                <p className="text">~~ {romanceRecord?.getTitle()} ~~</p>
+                <p className="text">{`"${romanceRecord?.getQuote()}"`}</p>
+                <h3 className="text">{romanceRecord?.getRedeemedResult()}</h3>
+                {hasAlreadyRedeemed && (
+                    <button className="button" onClick={toggleApplicationState}>
+                        Redeem your everyday treat 🎉😘
+                    </button>
+                )}
+            </div>
+        </>
+    );
 }
 export default App;
