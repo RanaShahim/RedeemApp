ae4e7ce001a8820b0ac86aef73bc710ae15a75ba
Shahim <shahim123321@gmail.com>
2024-02-15 16:15:21 +1100

    some refactor

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,13 @@ function App() {
         })(),
     );
 
+    const [totalPoints, setTotalPoints] = useState<number>(
+        (() => {
+            const storedPoints = localStorage.getItem('totalPoints');
+            return storedPoints ? parseInt(storedPoints) : 0;
+        })(),
+    );
+
     const setRomanceData = (): void => {
         const data = mapRomanceData();
         const defaultRecord = data.find(
@@ -23,8 +30,13 @@ function App() {
         const redeemableRecords = data.filter(
             (record) => record?.getId() !== 'default',
         );
+
         if (!hasAlreadyRedeemed) {
             const newRecord = redeemableRecords[currentIndex];
+            //This function is run anytime hasAlreadyRedeemed changes (when we click the redeem treat button), so we want to set the total in localStorage straight away
+            //and then read and when the hasAlreadyRedeemed is set to true again and the default page appears line 18 is run to reset
+            //the state. We can set the total the onclick event or here in the function.
+            localStorage.setItem('totalPoints', totalPoints.toString());
             setRomanceRecord(newRecord);
         } else {
             if (defaultRecord) {
@@ -35,8 +47,17 @@ function App() {
 
     const toggleApplicationState = (): void => {
         setHasAlreadyRedeemed(false);
+        //Set the default state to false so we can see the treat that was redeemded.
         localStorage.setItem('hasAlreadyRedeemed', JSON.stringify(false));
-        resetState();
+        const redeemableRecords = fetchRedeemableData();
+        const newIndex = Math.floor(Math.random() * redeemableRecords.length);
+        setCurrentIndex(newIndex);
+        const newRecord = redeemableRecords[newIndex];
+        //Set the new index everytime so when line 49 triggers a rerender, the state is updated.
+        localStorage.setItem('currentIndex', newIndex.toString());
+        setTotalPoints(
+            totalPoints > 99 ? 0 : totalPoints + newRecord?.getPoints(),
+        );
     };
 
     const resetState = (): void => {
@@ -46,18 +67,17 @@ function App() {
         tomorrow.setHours(0, 0, 0, 0);
         const timeUntilTomorrow = tomorrow.getTime() - now.getTime();
         setTimeout(() => {
-            const data = mapRomanceData();
-            const redeemableRecords = data.filter(
-                (record) => record?.getId() !== 'default',
-            );
-            const newIndex = Math.floor(
-                Math.random() * redeemableRecords.length,
-            );
-            setCurrentIndex(newIndex);
-            localStorage.setItem('currentIndex', newIndex.toString());
             setHasAlreadyRedeemed(true);
             localStorage.setItem('hasAlreadyRedeemed', JSON.stringify(true));
-        }, timeUntilTomorrow);
+        }, 4000);
+    };
+
+    const fetchRedeemableData = (): Romance[] => {
+        const data = mapRomanceData();
+        const redeemableRecords = data.filter(
+            (record) => record?.getId() !== 'default',
+        );
+        return redeemableRecords;
     };
 
     useEffect(() => {
@@ -79,6 +99,9 @@ function App() {
                 <p className="text">~~ {romanceRecord?.getTitle()} ~~</p>
                 <p className="text">{`"${romanceRecord?.getQuote()}"`}</p>
                 <h3 className="text">{romanceRecord?.getRedeemedResult()}</h3>
+                {hasAlreadyRedeemed && (
+                    <h3 className="text">😘Romance Points {totalPoints}😘</h3>
+                )}
                 {hasAlreadyRedeemed && (
                     <button className="button" onClick={toggleApplicationState}>
                         Redeem your everyday treat 🎉😘
